9b0035bb8b70faf057318dbc7ac896a6c1fe9fe0
Severine Coulon <severine.coulon@unine.ch>
2020-11-11 09:36:56 +0100

    texte : running pastry chef

--- dodger.py
+++ dodger.py
@@ -60,7 +60,7 @@ baddieImage = pygame.image.load('baddie.png')
 
 # Show the "Start" screen.
 windowSurface.fill(BACKGROUNDCOLOR)
-drawText('Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Running pastry chef', font, windowSurface, (WINDOWWIDTH / 3.6), (WINDOWHEIGHT / 3))
 drawText('Press a key to start.', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
 pygame.display.update()
 waitForPlayerToPressKey()
