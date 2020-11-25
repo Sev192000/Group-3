ed0a834f4221089c8f9dcfb3d99c30a46d92b62b
elsout <aubryelsa@yahoo.fr>
2020-11-25 10:05:05 +0100

    CHANGE NAME

--- dodger initial.py
+++ dodger initial.py
@@ -126,10 +126,10 @@ while True:
             GoodieAddcounter += 1
         if GoodieAddcounter == ADDNEWGOODIERATE:
             GoodieAddcounter = 0
-            baddieSize = random.randint(GOODIEMINSIZE, GOODIEMAXSIZE)
-            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH - baddieSize), 0 - baddieSize, baddieSize, baddieSize),
+            goodieSize = random.randint(GOODIEMINSIZE, GOODIEMAXSIZE)
+            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH - goodieSize), 0 - goodieSize, goodieSize, goodieSize),
                         'speed': random.randint(GOODIEMINSPEED, GOODIEMAXSPEED),
-                        'surface':pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
+                        'surface':pygame.transform.scale(baddieImage, (goodieSize, goodieSize)),
                         }
 
             goodies.append(newGoodie)
