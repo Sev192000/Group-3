5c8f694132c67624caba2f82cc6310d71431bc14
Severine Coulon <severine.coulon@unine.ch>
2020-12-09 10:27:46 +0100

    dodger test

--- dodger_test.py
+++ dodger_test.py
@@ -13,12 +13,28 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# player
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+# movement
+    def update(self):
+        self.rect.x += 5
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # Game loop
 running = True
@@ -32,7 +48,11 @@ while running:
             running = False
 
     # Update
+    all_sprites.update()
 
+    # Draw / render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
     # Draw / render
     screen.fill(BLACK)
     # *after* drawing everything, flip the display
